ee5a19cb07ace84ff798582c0d1c46e8c1d4f244
Vladyslav Depeshko <vladyslavdepeshko@MacBook-Air-Vladyslav.local>
2020-04-16 15:37:09 +0300

    slash symbols handler improved

--- src/execute/input_preprocessing/cmd_substitution/mx_cmd_substitution.c
+++ src/execute/input_preprocessing/cmd_substitution/mx_cmd_substitution.c
@@ -21,6 +21,14 @@ static int end_index_finder(char *expr, int start) {
     return end_index;
 }
 
+static int condition(char **cmd, int i, int j) {
+    if (j == 0 && cmd[i][j] == '`')
+        return 1;
+    else if (j != 0 && cmd[i][j - 1] != '\\' && cmd[i][j] == '`')
+        return 1;
+    return 0;
+}
+
 void mx_cmd_substitution(char **cmd, t_proc **proc) {
     char *res_of_cmd_exe;
     char *new_value;
@@ -31,7 +39,7 @@ void mx_cmd_substitution(char **cmd, t_proc **proc) {
     while(cmd[i]) {
         j = 0;
         while(cmd[i][j]) {
-            if (cmd[i][j] == '`' || (cmd[i][j] == '$' && cmd[i][j + 1] == '(')) {
+            if (condition(cmd, i, j)  || (cmd[i][j] == '$' && cmd[i][j + 1] == '(')) {
                 end_index      = end_index_finder(cmd[i], j);
                 res_of_cmd_exe = mx_subcmd_exe(cmd[i], j, end_index, proc);
                 new_value      = mx_str_edit(cmd[i], res_of_cmd_exe, j, end_index);
