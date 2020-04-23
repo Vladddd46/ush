1187cfbf649b65077ec25fca7338585fe2744609
Vladyslav Depeshko <vladyslavdepeshko@MacBook-Air-Vladyslav.local>
2020-04-23 17:12:44 +0300

    color built in functional made

--- src/execute/builtins/color/mx_color.c
+++ src/execute/builtins/color/mx_color.c
@@ -20,26 +20,77 @@ static int too_many_args_color(char **cmd_expression) {
     return 0;
 }
 
-void mx_color(char **cmd_expression) {
-    if (too_many_args_color(cmd_expression))
-        return;
-
+static char *bold(char **cmd_expression) {
     char *color = NULL;
 
-    if (mx_strcmp(cmd_expression[1], "yellow") == 0)
-        color = "\033[0;33m";
-    else if (mx_strcmp(cmd_expression[1], "red") == 0)
-        color = "\033[0;31m";
-    else if (mx_strcmp(cmd_expression[1], "green") == 0)
-        color = "\033[0;32m";
-    else if (mx_strcmp(cmd_expression[1], "blue") == 0)
-        color = "\033[0;34m";
-    
-    if (color != NULL)
-        write(1, color, mx_strlen(color));
+    if (mx_strcmp(cmd_expression[1], "bold_red")     == 0)
+        color = "\033[1;31m";
+    else if (mx_strcmp(cmd_expression[1], "bold_green")   == 0)
+        color = "\033[1;32m";
+    else if (mx_strcmp(cmd_expression[1], "bold_yellow")  == 0)
+        color = "\033[01;33m";
+    else if (mx_strcmp(cmd_expression[1], "bold_blue")    == 0)
+        color = "\033[1;34m";
+    else if (mx_strcmp(cmd_expression[1], "bold_magneta") == 0)
+        color = "\033[1;35m";
+    else if (mx_strcmp(cmd_expression[1], "bold_cyan")    == 0)
+        color = "\033[1;36m";
+    return color;
 }
 
+static char *not_bold(char **cmd_expression) {
+    char *color = NULL;
 
+    if (mx_strcmp(cmd_expression[1], "blue")         == 0)
+        color = "\033[0;34m";
+    else if (mx_strcmp(cmd_expression[1], "yellow")  == 0)
+        color = "\033[0;33m";
+    else if (mx_strcmp(cmd_expression[1], "red")     == 0)
+        color = "\033[0;31m";
+    else if (mx_strcmp(cmd_expression[1], "green")   == 0)
+        color = "\033[0;32m";
+    else if (mx_strcmp(cmd_expression[1], "magneta") == 0)
+        color = "\033[0;35m";
+    else if (mx_strcmp(cmd_expression[1], "cyan")    == 0)
+        color = "\033[0;36m";
+    else if (mx_strcmp(cmd_expression[1], "black")    == 0)
+        color = "\033[0m";
+    return color;
+}
 
+static void color_list() {
+    char *colors[16] = {"blue", "yellow", "red", 
+                        "green", "magneta", "cyan", 
+                        "black", "bold_red", "bold_green", 
+                        "bold_yellow", "bold_blue", "bold_magneta", 
+                        "bold_cyan", NULL};
+    int i = 0;
 
+    while(colors[i]) {
+        printf(">%s\n", colors[i]);
+        i++;
+    }
+}
+
+void mx_color(char **cmd_expression) {
+    char *color = NULL;
+    char *msg   = "color: no such color\n";
+    char *msg2  = "color: <color list> to see all colors\n";
+
+    if (too_many_args_color(cmd_expression))
+        return;
+    if (not_bold(cmd_expression)) 
+        color = not_bold(cmd_expression);
+    else if (bold(cmd_expression))
+        color = bold(cmd_expression);
+    
+    if (mx_strcmp(cmd_expression[1], "list") == 0)
+        color_list();
+    else if (color != NULL)
+        write(1, color, mx_strlen(color));
+    else {
+        write(2, msg, mx_strlen(msg));
+        write(2, msg2, mx_strlen(msg2));
+    }
+}
 
